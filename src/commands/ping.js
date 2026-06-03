10a274c19af6334ab39a4af5943d10357b2f1bf4
yyat0874-code <yyat0874@gmail.com>
2026-06-03 19:45:41 +0300

    Add error logging

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -3,10 +3,13 @@ module.exports = {
 
     async execute(message, client) {
 
+        const sent = await message.reply("🏓 Измеряю...");
+
+        const botPing = sent.createdTimestamp - message.createdTimestamp;
         const wsPing = client.ws.ping;
 
-        await message.reply(
-            `🏓 **Понг!**\n🤖 Пинг бота: ${wsPing} мс\n🌐 **Вебсокет:** ${wsPing} мс`
+        await sent.edit(
+            `🏓 **Понг!**\n🤖 **Пинг бота:** ${botPing} мс\n🌐 **Вебсокет:** ${wsPing} мс`
         );
     }
 };
