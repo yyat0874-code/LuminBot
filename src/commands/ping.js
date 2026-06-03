02328005de7e2ebcb9eac7f7637a5b4206c49b95
yyat0874-code <yyat0874@gmail.com>
2026-06-03 19:38:25 +0300

    Update ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,13 +2,11 @@ module.exports = {
     name: "ping",
 
     async execute(message, client) {
-        const sent = await message.reply("🏓 Измеряю...");
 
-        const apiPing = sent.createdTimestamp - message.createdTimestamp;
         const wsPing = client.ws.ping;
 
-        await sent.edit(
-            `🏓 Понг!\n📡 API: ${apiPing} мс\n🌐 Вебсокет: ${wsPing} мс`
+        await message.reply(
+            `🏓 **Понг!**\n🤖 Пинг бота: ${wsPing} мс\n🌐 **Вебсокет:** ${wsPing} мс`
         );
     }
 };
